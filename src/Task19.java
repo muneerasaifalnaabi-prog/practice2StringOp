c1725110297aa6742a32f4b1154b38e9ac697d11
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 16:46:29 +0400

    Task19 :Updated

--- src/Task19.java
+++ src/Task19.java
@@ -7,14 +7,17 @@ public class Task19 {
 
     public static String starOut(String str) {
         String result = "";
-
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '*') {
-                i++;
+            if ((i > 0 && str.charAt(i-1) == '*') ||
+                    str.charAt(i) == '*' ||
+                    (i + 1 < str.length() && str.charAt(i+1) == '*')) {
                 continue;
             }
             result += str.charAt(i);
         }
         return result;
     }
-}
+
+    }
+
+
