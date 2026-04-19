64f30494b386df61ddc75a6cf662f157e3f06bbd
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 10:16:18 +0400

    updated  logic in Task1

--- src/Task1.java
+++ src/Task1.java
@@ -1,16 +1,19 @@
 public class Task1 {
     public static void main(String[] args) {
+        String result = "The";
+        System.out.println(doubleChar("The"));
 
     }
     public static String doubleChar(String str ){
-
+        String result ="";
         for (int i=0 ;i<str.length();i++){
-            String result ="";
+
             char c =str.charAt(i);
             result+=c+c;
 
 
         }
+        return result;
 
     }
 }
