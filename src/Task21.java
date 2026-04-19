0a6a2eaa01494a685547b5a09318f5c4f44fb34c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 13:41:23 +0400

    updated class Task21

--- src/Task21.java
+++ src/Task21.java
@@ -1,8 +1,12 @@
 public class Task21 {
     public static void main(String[] args) {
+
+
+
+    }
+    public static String wordEnds(String str, String word) {
         String result = "";
         int pos = 0;
-
     }
 
 }
