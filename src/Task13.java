f4d04d7b9bef1ede4bbf00319f28e7ba882bb9f6
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:39:24 +0400

    Added class Task13

--- src/Task13.java
+++ src/Task13.java
@@ -1,6 +1,9 @@
 public class Task13 {
     public static void main(String[] args) {
-
+        System.out.println(prefixAgain());
+    }
+    public static boolean prefixAgain(String str, int n) {
+        return str.substring(n).contains(str.substring(0,n));
     }
 
 }
