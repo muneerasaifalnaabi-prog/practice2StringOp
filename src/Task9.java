891a6fc643767e4a3f8e95c29d0fe6ce1cdaf083
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:15:52 +0400

    Updated  class Task9

--- src/Task9.java
+++ src/Task9.java
@@ -7,6 +7,14 @@ public class Task9 {
     public static String mixString(String a, String b) {
         String result = "";
         int i = 0;
+        while (i < a.length() && i < b.length()) {
+            result += a.charAt(i) + b.charAt(i);
+            i++;
+        }
+        result += a.substring(i) + b.substring(i);
+        return result;
     }
+
+
 
 }
