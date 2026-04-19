bb67a4c03afa5d055c3ce165ee68cac04bb7b35c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 12:08:00 +0400

    Updated in class16

--- src/Task16.java
+++ src/Task16.java
@@ -6,10 +6,14 @@ public class Task16 {
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == '*') {
                 if (i == 0 || i == str.length() - 1) {
-                    return false;  // * at start/end invalid
+                    return false;
                 }
-
+                if (str.charAt(i - 1) != str.charAt(i + 1)) {
+                    return false;
+                }
             }
+            }
+        return true;
         }
     }
-}
+
