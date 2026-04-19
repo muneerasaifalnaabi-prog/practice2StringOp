3e05c455c61a2f281598e999fab6d9f83081b06d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:49:51 +0400

    updated  class Task 14

--- src/Task14.java
+++ src/Task14.java
@@ -5,6 +5,10 @@ public class Task14 {
     public boolean xyzMiddle(String str) {
         int mid = str.length()/2;
 
+        if(!str.contains("xyz")){
+            return false;
+        }
+
     }
 
 }
