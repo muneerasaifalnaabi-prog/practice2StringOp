7c051c1d2fbf71f05a4555da7874750369870e33
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:58:04 +0400

    Updated in class Task15

--- src/Task15.java
+++ src/Task15.java
@@ -6,6 +6,9 @@ public class Task15 {
         int first = str.indexOf("bread");
         if (first == -1) return "";
         int last = str.indexOf("bread", first + 1);
+        if (last == -1) return "";
+
+
 
 
 
