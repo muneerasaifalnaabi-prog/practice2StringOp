978be013de98e839fa7caccfc5c2ce956f7107ce
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 10:27:19 +0400

    added Task3 class

--- src/Task3.java
+++ src/Task3.java
@@ -2,5 +2,11 @@ public class Task3 {
     public static void main(String[] args) {
 
     }
+    public static Boolean catDog(String str ){
+
+        for (int i=0 ;i<str.length();i++){
+
+        }
+    }
 
 }
