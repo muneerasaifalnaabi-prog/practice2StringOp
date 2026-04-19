a55ed501df3b2feb4b61bb8f6b6fe9ddb6a7aabb
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 10:45:03 +0400

    added class Task5

--- src/Task5.java
+++ src/Task5.java
@@ -1,6 +1,6 @@
 public class Task5 {
     public static void main(String[] args) {
-
+        System.out.println(endOther("Hiabc", "abc"));
 
     }
     public static Boolean endOther(String str ,String text){.
