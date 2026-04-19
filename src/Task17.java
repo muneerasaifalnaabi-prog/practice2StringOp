9e64a1c5f8b8f45166f43f31ace0c2fc8b5385aa
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 12:18:05 +0400

    updated class Task17

--- src/Task17.java
+++ src/Task17.java
@@ -1,12 +1,17 @@
 public class Task17 {
     public static void main(String[] args) {
-
+        System.out.println(oneTwo("abc"));
     }
     public static String oneTwo(String str) {
         String result = "";
         for (int i = 0; i < str.length(); i += 3) {
             if (i + 2 < str.length()) {
-
+                result += str.charAt(i + 1) + str.charAt(i + 2) + str.charAt(i);
             }
+            else {
+                result += str.substring(i);
+            }
+            }
+        return result;
     }
 }
