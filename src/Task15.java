1c52b5b5472f3007703f835f20f155fd329f6a58
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:57:46 +0400

    Updated in class Task15

--- src/Task15.java
+++ src/Task15.java
@@ -4,6 +4,10 @@ public class Task15 {
     }
     public static String getSandwich(String str) {
         int first = str.indexOf("bread");
+        if (first == -1) return "";
+        int last = str.indexOf("bread", first + 1);
+
+
 
     }
 }
