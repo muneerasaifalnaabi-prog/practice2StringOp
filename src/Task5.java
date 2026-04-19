e0735302bf9ae9e751abfe3208a3e97b15f4e0db
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 10:46:36 +0400

    Task5 :f one string appears at the end of the other completed

--- src/Task5.java
+++ src/Task5.java
@@ -1,9 +1,10 @@
 public class Task5 {
     public static void main(String[] args) {
         System.out.println(endOther("Hiabc", "abc"));
+        System.out.println(endOther("Zelda", "da"));
 
     }
-    public static Boolean endOther(String str ,String text){.
+    public static Boolean endOther(String str ,String text){
 
         str= str.toLowerCase();
         text = text.toLowerCase();
