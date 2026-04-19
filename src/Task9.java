a6719c48977a7e0bb0d34da7dcf9a813d48d3dda
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:17:17 +0400

    Task9 :Merge and alternating characters completed

--- src/Task9.java
+++ src/Task9.java
@@ -1,6 +1,7 @@
 public class Task9 {
     public static void main(String[] args) {
         System.out.println(mixString("abc", "xyz"));
+        System.out.println(mixString("Hi", "There"));
 
 
 }
