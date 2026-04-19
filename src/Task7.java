d9582e1727a25c2b9454bd3eff313f7881492cba
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:00:25 +0400

    Task 7 :Check pattren completed

--- src/Task7.java
+++ src/Task7.java
@@ -1,13 +1,13 @@
 public class Task7 {
     public static void main(String[] args) {
         System.out.println(bobThere("abcbob"));
+        System.out.println(bobThere("bac"));
 
     }
     public static Boolean bobThere(String str ){
         for (int i=0 ;i<str.length();i++){
             if (str.charAt(i) == 'b' &&
-                    str.charAt(i + 1) == 'o' &&
-                    str.charAt(i + 3) == 'b') {
+                    str.charAt(i + 2) == 'b') {
                return true;
             }
 
