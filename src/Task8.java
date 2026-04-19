32d81a711fdbf0ab1eba623c009e8829302c9148
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:03:06 +0400

    Added class Task8

--- src/Task8.java
+++ src/Task8.java
@@ -3,6 +3,15 @@ public class Task8 {
 
     }
     public static Boolean bobThere(String str ){
+        for (int i=0 ;i<str.length();i++){
+            if (str.charAt(i) == 'x' &&
+                    str.charAt(i + 1) == 'y') {
+                return true;
+            }
+
+        }
+        return false;
+    }
 
     }
 }
