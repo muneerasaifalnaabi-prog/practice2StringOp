7af0d659202fa6b7901e0eda2f5d192e58d1d13b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:59:21 +0400

    Updated in class Task15

--- src/Task15.java
+++ src/Task15.java
@@ -1,5 +1,6 @@
 public class Task15 {
     public static void main(String[] args) {
+        System.out.println(getSandwich("breadjambread"));
 
     }
     public static String getSandwich(String str) {
