20d7feac2dc867a12b28709db96f6287acd80975
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:09:21 +0400

    Task8 :xy-balanced  completed

--- src/Task8.java
+++ src/Task8.java
@@ -2,14 +2,13 @@ public class Task8 {
     public static void main(String[] args) {
         System.out.println(xyBalance("aaxbby"));
 
+
     }
     public static Boolean xyBalance(String str ) {
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'y') {
                 return true;
-
             }
-
         }
         return false;
     }
