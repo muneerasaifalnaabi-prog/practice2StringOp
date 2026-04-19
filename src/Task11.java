8e1f0d133750fffd0f0b1f4d20fa6864fd49bc2f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:28:05 +0400

    updated class Task11

--- src/Task11.java
+++ src/Task11.java
@@ -4,6 +4,13 @@ public class Task11 {
 
     }
     public static String repeatFront(String str, int n) {
+        String result = "";
+        int prefixLen = n;
+        while (prefixLen > 0) {
+            result += str.substring(0, prefixLen);
+            prefixLen--;
+        }
+
 
     }
 }
