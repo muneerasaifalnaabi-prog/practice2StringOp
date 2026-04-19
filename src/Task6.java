27393f35912e89010a6cd3f4d973f09d74346032
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 10:53:14 +0400

    Task6 : xyzThere completed

--- src/Task6.java
+++ src/Task6.java
@@ -1,6 +1,7 @@
 public class Task6 {
     public static void main(String[] args) {
         System.out.println(xyzThere("abcxyz"));
+        System.out.println(xyzThere("abc.xyz"));
 
     }
     public static Boolean xyzThere(String str ){
