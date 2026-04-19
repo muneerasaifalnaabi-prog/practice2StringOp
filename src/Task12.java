33fa1d54e950125000d4434e20a82bc4542573bc
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:32:55 +0400

    Updated  class Task 12

--- src/Task12.java
+++ src/Task12.java
@@ -2,8 +2,10 @@ public class Task12 {
     public static void main(String[] args) {
 
     }
+
     public static String repeatSeparator(String word, String sep, int count) {
-
+        String result = "";
     }
-
 }
+
+
