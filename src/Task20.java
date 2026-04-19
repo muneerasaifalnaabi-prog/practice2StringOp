4b3a4cb24f697f15cedb15169917e42f992479b7
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 12:58:40 +0400

    Task20 :Replace all characterscompleted

--- src/Task20.java
+++ src/Task20.java
@@ -1,2 +1,21 @@
 public class Task20 {
-}
+    public static void main(String[] args) {
+
+        System.out.println(plusOut("12xy34", "xy"));
+    }
+    public static String plusOut(String str, String word) {
+        String result = str.replaceAll(".", "+");
+        int i = 0;
+        while (i <= str.length() - word.length()) {
+            if (str.substring(i, i + word.length()).equals(word)) {
+                result = result.substring(0, i) + word +
+                        result.substring(i + word.length());
+                i += word.length();
+            } else {
+                i++;
+            }
+        }
+        return result;
+    }
+    }
+
