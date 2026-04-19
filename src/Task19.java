d54b2ee196f851961f1aa73ea7f57928a6158c05
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 12:34:14 +0400

    updated  class Task19

--- src/Task19.java
+++ src/Task19.java
@@ -6,5 +6,11 @@ public class Task19 {
     public static String starOut(String str) {
         String result = "";
 
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*') {
+                i++;
+                continue;
+            }
+        }
     }
 }
