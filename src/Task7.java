1c21d403c57792f0089cb72d222badab3e584c5e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 10:57:54 +0400

    updated class Task7

--- src/Task7.java
+++ src/Task7.java
@@ -1,5 +1,17 @@
 public class Task7 {
     public static void main(String[] args) {
+        System.out.println(bobThere("abcbob"));
 
+    }
+    public static Boolean bobThere(String str ){
+        for (int i=0 ;i<str.length();i++){
+            if (str.charAt(i) == 'b' &&
+                    str.charAt(i + 1) == 'o' &&
+                    str.charAt(i + 3) == 'b') {
+               return true;
+            }
+
+        }
+        return false;
     }
 }
