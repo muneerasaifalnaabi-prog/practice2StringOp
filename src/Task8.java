f50a67a9d403feac4e9d8f49216c9e8a686877b9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 11:07:37 +0400

    Added class Task8

--- src/Task8.java
+++ src/Task8.java
@@ -1,17 +1,17 @@
 public class Task8 {
     public static void main(String[] args) {
+        System.out.println(xyBalance("aaxbby"));
 
     }
-    public static Boolean bobThere(String str ){
-        for (int i=0 ;i<str.length();i++){
-            if (str.charAt(i) == 'x' &&
-                    str.charAt(i + 1) == 'y') {
+    public static Boolean xyBalance(String str ) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'y') {
                 return true;
+
             }
 
         }
         return false;
     }
-
     }
-}
+
