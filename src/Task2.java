48f4e590bbb432e7d8594610659089f80b007721
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 10:22:30 +0400

    updated  class Task2

--- src/Task2.java
+++ src/Task2.java
@@ -5,6 +5,7 @@ public class Task2 {
     public static String countHi(String str ){
         int count =0;
         for (int i=0 ;i<str.length();i++){
+            if( str.charAt(i)=='h' && )
 
         }
 
